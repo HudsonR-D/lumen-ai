629116d781c97ab31452052ee31c38bb88ffc22f
Lumen Symbiosis <lumen-symbiosis@users.noreply.github.com>
2026-05-14 14:05:43 +0000

    Update Lumen visual archive

--- lumens/visuals/gallery/manifest.js
+++ lumens/visuals/gallery/manifest.js
@@ -1,6 +1,6 @@
 window.LUMEN_VISUALS_MANIFEST = {
-  "generatedAt": "2026-05-14T04:01:36.911878+00:00",
-  "latestCycle": 277,
+  "generatedAt": "2026-05-14T08:47:23.542287+00:00",
+  "latestCycle": 278,
   "knownArtifacts": [],
   "artifactPatterns": [
     "cycle-{cycle}-combined.png",
